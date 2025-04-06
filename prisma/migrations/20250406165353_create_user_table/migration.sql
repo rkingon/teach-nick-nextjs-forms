7e22a1acb02cd53c4774f0a6b9a109b12ea4a863
Roi Kingon <rkingon@gmail.com>
2025-04-06 09:55:03 -0700

    add user migration file

---
+++ prisma/migrations/20250406165353_create_user_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
